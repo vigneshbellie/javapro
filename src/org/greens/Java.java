3e9c4c4350d2e985f1a7be0061fce1a13a80c88a
vigneshbellie <vigneshprismsurface@gmail.com>
2022-01-13 11:53:46 +0530

    done my coding

--- src/org/greens/Java.java
+++ src/org/greens/Java.java
@@ -1,5 +1,10 @@
 package org.greens;
 
 public class Java {
-
+public static void main(String[] args) {
+	System.out.println("my name is vignesh");
+	System.out.println("my fathers name is bellie");
+	System.out.println("my mothers name is manjula");
+	System.out.println("my grandmothers name is patchiammal");
+}
 }
